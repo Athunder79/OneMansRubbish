b1d9c2d2107ea1911df4c12975c90606bbfd859b
Athunder79 <athunder@me.com>
2023-11-08 14:33:13 +0000

    hide button

--- OneMansRubbish/static/script/script.js
+++ OneMansRubbish/static/script/script.js
@@ -1,7 +1,18 @@
 addEventListener('load', function () {
+    hideClaimButton();
     document.getElementById("add-comment").addEventListener("click",openComment );
     document.getElementById("submit-comment").addEventListener("click",closeComment );
     document.getElementById("cancel-comment").addEventListener("click",closeComment );
+    document.getElementById("claimButton").addEventListener("click",removeClaimButton );
+
+    function hideClaimButton() {
+        if (document.getElementById("status").innerText == "Available") {
+            document.getElementById("claimButton").className = "show";
+        }
+        else {
+            (document.getElementById("claimButton").className = "hide");
+        }
+    }
 })
 
 function openComment() {
@@ -12,4 +23,8 @@ function openComment() {
 function closeComment() {
     document.getElementById("commentForm").className = "hide";
     document.getElementById("add-comment").className = "show";
+}
+
+function removeClaimButton() {
+    document.getElementById("claimButton").className = "hide";
 }
